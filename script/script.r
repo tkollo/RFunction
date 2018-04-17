93d298b1475d0000ea45bca3408cc931a5c71a51
TK <tkollo@gmail.com>
2018-04-17 23:48:36 +0200

    b

--- script/script.r
+++ script/script.r
@@ -15,12 +15,5 @@ atoken <- credentials$twitter$access_token
 asecret <- credentials$twitter$access_secret
 
 setup_twitter_oauth(ckey, csecret, atoken, asecret)
-if (Sys.getenv('WEBSITE_COMPUTE_MODE') == 'Dynamic') {
-  # The graphics libraries on the consumption mode are not available
-  # so you cannot generate a graph
-  tweet(paste("Simplify"))  
-} else {
-  tweet(paste("Simplify"), mediaPath = plot_file_png)
-}
 
-#  ------------------------------------------------------------------------
+tweet(paste("Simplify")) 
