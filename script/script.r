3878f6d434cfe2257cb6ed1e6c71e745815ff87c
TK <tkollo@gmail.com>
2018-04-17 20:02:23 +0200

    separate installation
    installation separation

--- script/script.r
+++ script/script.r
@@ -1,13 +1,3 @@
-options(repos = "https://mran.microsoft.com")
-
-# Installing packages ------------------------------------------------------------------
-
-if (!require('httr')) install.packages('httr')
-if (!require('jsonlite')) install.packages('jsonlite')
-if (!require('ggplot2')) install.packages('ggplot2')
-if (!require('twitteR')) install.packages("twitteR")
-if (!require('rsvg')) install.packages("rsvg")
-if (!require('rjson')) install.packages("rjson")
 library('rjson')
 library('rsvg')
 library('twitteR')
