cf648b5923b866b449ee947cefc4f031b6b55e8c
TK <tkollo@gmail.com>
2018-04-18 00:40:55 +0200

    installations

--- TriggerTimerFunction/run.ps1
+++ TriggerTimerFunction/run.ps1
@@ -1,3 +1,3 @@
 cd D:\home\site\wwwroot\script
-D:\home\R-3.3.3\bin\Rscript.exe installation.r 2>&1
-D:\home\R-3.3.3\bin\Rscript.exe script.r 2>&1
+D:\home\R-3.3.3\bin\x64\Rscript.exe installations.r 2>&1
+D:\home\R-3.3.3\bin\x64\Rscript.exe script.r 2>&1
