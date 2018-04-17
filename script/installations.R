195d5f1a17216df4431a28602a4bd024c5d1a9a3
TK <tkollo@gmail.com>
2018-04-17 20:14:38 +0200

    install

--- script/installations.R
+++ script/installations.R
@@ -1,1 +1,6 @@
-install.packages('httr', dependencies=TRUE, repos='http://cran.rstudio.com/')
+if (!require('httr')) install.packages('httr', dependencies=TRUE, repos='http://cran.rstudio.com/')
+if (!require('rjson')) install.packages('rjson', dependencies=TRUE, repos='http://cran.rstudio.com/')
+if (!require('rsvg')) install.packages('rsvg', dependencies=TRUE, repos='http://cran.rstudio.com/')
+if (!require('twitteR')) install.packages('twitteR', dependencies=TRUE, repos='http://cran.rstudio.com/')
+if (!require('jsonlite')) install.packages('jsonlite', dependencies=TRUE, repos='http://cran.rstudio.com/')
+if (!require('ggplot2')) install.packages('ggplot2', dependencies=TRUE, repos='http://cran.rstudio.com/')
